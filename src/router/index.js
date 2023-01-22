b163339efba9af2d16282f42f39160200b1935a5
deez-blip <gaspard.castagnet92@gmail.com>
2023-01-22 19:42:21 +0100

    projet vue.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,30 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+// import HomeView from '../views/HomeView.vue'
+import ClavierView from '../views/ClavierView.vue'
+import ContactView from '../views/ContactView.vue'
+import JournalView from '../views/JournalView.vue'
+import AddContactView from '../views/AddContactView.vue'
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
+    path: '/clavier',
+    name: 'clavier',
+    component: ClavierView
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/contact',
+    name: 'contact',
+    component: ContactView
+  },
+  {
+    path: '/journal',
+    name: 'journal',
+    component: JournalView
+  },
+  {
+    path: '/addContact',
+    name: 'addContact',
+    component: AddContactView
   }
 ]
 
--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,34 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    contacts: [
+      {
+        nom: 'test',
+        numero: '0000000000'
+      },
+      {
+        nom: 'test2',
+        numero: '1111111111'
+      },
+    ],
+    journal: [
+    ],
+    associateNumber: [
+
+    ]
   },
   getters: {
   },
   mutations: {
+    addContact(state, formContact) {
+      state.contacts.push(formContact)
+    },
+    addJournal(state, formJournal) {
+      state.journal.push(formJournal)
+    },
+    addAssociateNumber(state, val) {
+      state.associateNumber.push(val)
+    }
   },
   actions: {
   },
